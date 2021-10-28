97f2345c0404189f4d6b3e73241cad54f251416d
Eric Zacarias <44508016+ez98@users.noreply.github.com>
2021-10-28 18:33:04 -0500

    comments added

--- StudySpaceDemo/src/components/SpaceInfo.js
+++ StudySpaceDemo/src/components/SpaceInfo.js
@@ -36,16 +36,6 @@ export default class SpaceInfo extends React.Component {
       this.setState({status:data[i]['status']});
     });
   }
-//   getRandStatus(){
-//     const i = Math.floor(Math.random() * 15);
-
-//     if ( i % 2 != 0 ) {
-//       return "Available"
-//     }
-//     else{
-//       return "Occupied"
-//     }
-//   }
 
 
   componentDidMount() {
