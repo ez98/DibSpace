61c764988b6ad4e647d6afed80f8b6a3627672d4
Sameer Rangoonwala <srangoon@hawk.iit.edu>
2021-11-02 08:50:00 -0500

    Updated Tables.sql
    Changed "reservation_id", "study_space_id", and "room_id" datatypes from int to varchar to allow  leading 0's
    
    Changed "seats" and "table" attributes in study_space table to type boolean
    
    Renamed "status" attribute in study_space table to "available" and changed its datatype to boolean
    
    Renamed "availablility" attribute in room table to "available" and changed its datatype to boolean

--- study-room-backend/SQL Code/Tables.sql
+++ study-room-backend/SQL Code/Tables.sql
@@ -9,16 +9,24 @@ CREATE TABLE `building` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `reservation` (
-  `reservation_id` int NOT NULL,
+  `reservation_id` varchar(6) NOT NULL,
   `student_id` int NOT NULL,
-  `study_space_id` int NOT NULL,
+  `study_space_id` varchar(6) NOT NULL,
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
   PRIMARY KEY (`reservation_id`),
   KEY `study_space_id_idx` (`study_space_id`),
   KEY `student_id_idx` (`student_id`),
-  CONSTRAINT `student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
-  CONSTRAINT `study_space_id` FOREIGN KEY (`study_space_id`) REFERENCES `study_space` (`study_space_id`)
+  CONSTRAINT `student_id` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `room` (
+  `room_id` varchar(6) NOT NULL,
+  `building_tag` varchar(45) NOT NULL,
+  `available` tinyint NOT NULL,
+  PRIMARY KEY (`room_id`),
+  KEY `building_tag_idx` (`building_tag`),
+  CONSTRAINT `building_tag` FOREIGN KEY (`building_tag`) REFERENCES `building` (`building_tag`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `student` (
@@ -28,23 +36,13 @@ CREATE TABLE `student` (
   PRIMARY KEY (`student_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `room` (
-  `room_id` int NOT NULL,
-  `building_tag` varchar(45) NOT NULL,
-  `availability` varchar(45) NOT NULL,
-  PRIMARY KEY (`room_id`),
-  KEY `building_tag_idx` (`building_tag`),
-  CONSTRAINT `building_tag` FOREIGN KEY (`building_tag`) REFERENCES `building` (`building_tag`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `study_space` (
-  `study_space_id` int NOT NULL,
-  `room_id` int NOT NULL,
+  `study_space_id` varchar(6) NOT NULL,
+  `room_id` varchar(6) NOT NULL,
   `qr_code` varchar(45) NOT NULL,
-  `seats` int NOT NULL,
-  `table` varchar(3) NOT NULL,
-  `status` varchar(45) NOT NULL,
+  `seats` tinyint NOT NULL,
+  `table` tinyint NOT NULL,
+  `available` tinyint NOT NULL,
   PRIMARY KEY (`study_space_id`),
-  KEY `room_id_idx` (`room_id`),
-  CONSTRAINT `room_id` FOREIGN KEY (`room_id`) REFERENCES `room` (`room_id`)
+  KEY `room_id_idx` (`room_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
